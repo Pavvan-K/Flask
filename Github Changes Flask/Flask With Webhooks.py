30eae4bc37cc02e16ee613ea6f5b2cd8cede5cfb
K Pavan <pavan4777@gmail.com>
2019-03-23 12:48:06 +0530

    Flask app to get Github Changes

---
+++ Github Changes Flask/Flask With Webhooks.py
@@ -0,0 +1,44 @@
+## IT will notice the changes if something happends in my git repo. (pavan228)
+# coding: utf-8
+
+# In[1]:
+
+
+from flask import Flask
+from flask import json
+from flask import request
+
+
+# In[2]:
+
+
+app = Flask(__name__)
+
+
+# In[3]:
+
+
+@app.route("/")
+def my_def():
+    return "Hello Its working!"
+@app.route("/git-hub",methods=['POST'])
+def my_github_change():
+	if request.headers['Content-Type']=="application/json":
+		global my_info
+		
+		my_info = json.dumps(request.json)
+		#print(my_info)
+		with open("payload.txt",'w') as f:
+			f.write(my_info)
+		print("Payload Saved!")
+		print("Created: ",json.dumps(request.json)['build']['created_at'])
+		print("URL",json.dumps(request.json)['pusher']['url'])
+		return my_info
+
+#with open("payload.txt",'w') as f:
+	#f.write(my_info)
+	
+
+if __name__=="__main__":
+    app.run(debug=True)
+
